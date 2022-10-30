5098aff93bd0da7fbf4768f64696df1cd66c9a8b
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2022-10-30 23:22:08 +0700

    change content

--- ken/src/Data.js
+++ ken/src/Data.js
@@ -1,28 +1,28 @@
 let shopItemsData = [
   {
     id: "jfhgbvnscs",
-    name: "Casual Shirt",
+    name: "Cup Cake",
     price: 45,
     desc: "หอมอร่อยมั้ง",
     img: "https://www.noracooks.com/wp-content/uploads/2022/03/sq-3.jpg",
   },
   {
     id: "ioytrhndcv",
-    name: "Office Shirt",
+    name: "Chocolate Cake",
     price: 100,
     desc: "ดีที่ไม่เน่า",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-xbbtOjenth-o4vYlvVB_CUXp40wW3CF2pg&usqp=CAU",
   },
   {
     id: "wuefbncxbsn",
-    name: "T Shirt",
+    name: "Crepe Cake",
     price: 25,
     desc: "อย่าทิ้งเกิน 2 วันไม่งั้นมดขึ้น",
     img: "https://annabanana.co/wp-content/uploads/2020/01/Chocolate-Crepe-Cake-Feat-1.jpg",
   },
   {
     id: "thyfhcbcv",
-    name: "Mens Suit",
+    name: "Brownny",
     price: 300,
     desc: "กินวันนี้ ขี้พน.",
     img: "https://scontent.xx.fbcdn.net/v/t1.15752-9/307844698_1297173247777551_2711877312343039912_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=aee45a&_nc_ohc=PU0P3RFvM8cAX_D25Vn&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTWipSHqEMLiycKZtzWw6pn6cWiKLJCveKwrqNWXH6zyA&oe=637DB93E",
