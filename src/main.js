84d843e5d2410c425fcacd0d9c0b344a15b4ac54
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2022-10-31 00:17:39 +0700

    edit js file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,36 @@
-let shop = document.getElementById("shop");
-
+let shop = document.getElementById("Bestsell");
+let shop2 = document.getElementById("new");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop = () => {
-  return (shop.innerHTML = shopItemsData
+  return (shop.innerHTML = BestSeller
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      return `
+    <div id=product-id-${id} class="item">
+        <img width="220" src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${price} </h2>
+            <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">
+              ${search.item === undefined ? 0 : search.item}
+              </div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    })
+    .join(""));
+};
+let generatenew = () => {
+  return (shop2.innerHTML = neww
     .map((x) => {
       let { id, name, price, desc, img } = x;
       let search = basket.find((x) => x.id === id) || [];
@@ -31,6 +58,7 @@ let generateShop = () => {
 };
 
 generateShop();
+generatenew();
 
 let increment = (id) => {
   let selectedItem = id;
