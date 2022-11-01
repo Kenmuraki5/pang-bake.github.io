7e0cd2f64034797161adafe2a16c7135d5d615cb
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2022-11-02 05:48:30 +0700

    re

--- src/Data.js
+++ src/Data.js
@@ -58,3 +58,106 @@ let neww = [
     img: "https://i8b2m3d9.stackpathcdn.com/wp-content/uploads/2020/08/Strawberry_Shortcake_5140sq.jpg",
   },
 ];
+let ButterCake = [
+  {
+      id: "Butter1",
+      name: "Cup Cake",
+      price: 45,
+      desc: "หอมอร่อยมั้ง",
+      img: "https://www.noracooks.com/wp-content/uploads/2022/03/sq-3.jpg",
+  },
+  {
+      id: "Butter2",
+      name: "Chocolate Cake",
+      price: 100,
+      desc: "ดีที่ไม่เน่า",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-xbbtOjenth-o4vYlvVB_CUXp40wW3CF2pg&usqp=CAU",
+  },
+  {
+      id: "Butter3",
+      name: "Crepe Cakes",
+      price: 25,
+      desc: "อย่าทิ้งเกิน 2 วันไม่งั้นมดขึ้น",
+      img: "https://annabanana.co/wp-content/uploads/2020/01/Chocolate-Crepe-Cake-Feat-1.jpg",
+  },
+  {
+      id: "Butter4",
+      name: "Brownny",
+      price: 300,
+      desc: "กินวันนี้ ขี้พน.",
+      img: "https://scontent.xx.fbcdn.net/v/t1.15752-9/307844698_1297173247777551_2711877312343039912_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=aee45a&_nc_ohc=PU0P3RFvM8cAX_D25Vn&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTWipSHqEMLiycKZtzWw6pn6cWiKLJCveKwrqNWXH6zyA&oe=637DB93E",
+  },
+];
+
+// page2
+let ButterCake2 = [
+  {
+      id: "Butter5",
+      name: "Cup Cake",
+      price: 45,
+      desc: "หอมอร่อยมั้ง",
+      img: "https://www.noracooks.com/wp-content/uploads/2022/03/sq-3.jpg",
+  },
+  {
+      id: "Butter6",
+      name: "Chocolate Cake",
+      price: 100,
+      desc: "ดีที่ไม่เน่า",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-xbbtOjenth-o4vYlvVB_CUXp40wW3CF2pg&usqp=CAU",
+  },
+  {
+      id: "Butter7",
+      name: "Crepe Cakes",
+      price: 25,
+      desc: "อย่าทิ้งเกิน 2 วันไม่งั้นมดขึ้น",
+      img: "https://annabanana.co/wp-content/uploads/2020/01/Chocolate-Crepe-Cake-Feat-1.jpg",
+  },
+];
+
+
+let icecreamecake = [
+  {
+      id: "icescreamcake1",
+      name: "Matcha Crepe Cake",
+      price: 50,
+      desc: "หอมหวาน",
+      img: "https://www.indulgewithmimi.com/wp-content/uploads/2018/10/matcha-green-tea-mille-pancake-crepe-cake-japanese-1-735x735.jpg",
+  },
+  {
+      id: "icescreamcake2",
+      name: "Egg Cake",
+      price: 80,
+      desc: "ขนมไข่",
+      img: "https://scontent.xx.fbcdn.net/v/t1.15752-9/308692464_626747699129576_4075605894809072116_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_ohc=L2TXVSKsih8AX9N2oQQ&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSLMQkuagoSlomKCjXpI0w0tnB8FNX629iFoT2tteH2Uw&oe=63844283",
+  },
+  {
+      id: "icescreamcake3",
+      name: "Dog pang",
+      price: 75,
+      desc: "ขมปังหน้าหมา",
+      img: "https://pbs.twimg.com/media/Et2M2BAUcAAOCuN.jpg",
+  },
+  {
+      id: "icescreamcake4",
+      name: "Strawberry Cake",
+      price: 400,
+      desc: "อร่อยลืมเมีย",
+      img: "https://i8b2m3d9.stackpathcdn.com/wp-content/uploads/2020/08/Strawberry_Shortcake_5140sq.jpg",
+  },
+];
+let icecreamecake2 = [
+  {
+      id: "icescreamcake5",
+      name: "Matcha Crepe Cake",
+      price: 50,
+      desc: "หอมหวาน",
+      img: "https://www.indulgewithmimi.com/wp-content/uploads/2018/10/matcha-green-tea-mille-pancake-crepe-cake-japanese-1-735x735.jpg",
+  },
+  {
+      id: "icescreamcake6",
+      name: "Egg Cake",
+      price: 80,
+      desc: "ขนมไข่",
+      img: "https://scontent.xx.fbcdn.net/v/t1.15752-9/308692464_626747699129576_4075605894809072116_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_ohc=L2TXVSKsih8AX9N2oQQ&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSLMQkuagoSlomKCjXpI0w0tnB8FNX629iFoT2tteH2Uw&oe=63844283",
+  },
+];
