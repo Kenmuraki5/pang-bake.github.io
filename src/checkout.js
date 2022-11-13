5cf12d041d8fa7c42f67d6c383b1c0760f648cb2
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2022-11-13 21:34:54 +0700

    update

--- src/checkout.js
+++ src/checkout.js
@@ -21,24 +21,16 @@ let generateCartItems = () => {
         snack.find((x) => x.id === id) ||[];
         return `
       <div class="cart-item">
-        <img width="100" src=${search.img} alt="" />
-        <div class="details">
-
-          <div class="title-price-x">
+        <img width="120px" src=${search.img} alt="" />
+        <div>
+          <div class="title-price-x m-2">
               <h4 class="title-price">
                 <p>${search.name}</p>
               </h4>
-              <p class="cart-item-price">$ ${search.price}</p>
-              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
           </div>
+          <div id=${id} class="quantity m-2">amount : ${item}</div>
 
-          <div class="buttons">
-              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id=${id} class="quantity">${item}</div>
-              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-          </div>
-
-          <h3>$ ${item * search.price}</h3>
+          <h3 class="m-2">$ ${item * search.price}</h3>
         </div>
       </div>
       `;
