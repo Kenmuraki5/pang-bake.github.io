8620670450816c8fea97e5ba6754dd575ea06057
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2022-11-16 08:33:52 +0700

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -18,10 +18,10 @@
 <?php
     session_start();
     if(empty($_SESSION["Username"])){
-        echo '<script type="text/javascript">';
-            echo 'alert("You have taken too long to pay. Please login again.")';  
-        echo '</script>';
-        header("Location: login.php");
+        echo '<script">';
+            echo "alert('You have taken too long to pay. Please login again.')";
+            echo "window.location.replace('login.php');";  
+        echo '</script>';;
     }
 ?>
 <body>
