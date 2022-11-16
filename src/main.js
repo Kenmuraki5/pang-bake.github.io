9503d2a538c09c493c24db0784f215bab68e365e
Kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2022-11-16 14:32:10 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -44,7 +44,7 @@ let generateShop = () => {
       let search = basket.find((x) => x.id === id) || [];
       return `
     <div id=product-id-${id} class="item">
-        <a href="detail.html"><img width="100%" alt="${name}" src=${img} id="${id}" name="${price}" value="${desc}"onclick="savedata(this)"></a>
+        <a href="detail.html"><img width="100%" alt="${name}" src=${img} id="${id}" name="${price}" value="${desc}" onclick="savedata(this)"></a>
         <div class="details">
           <h3>${name}</h3>
           <div class="price-quantity">
@@ -69,7 +69,7 @@ let generatenew = () => {
       let search = basket.find((x) => x.id === id) || [];
       return `
     <div id=product-id-${id} class="item">
-        <a href="detail.html"><img width="100%" src=${img} id="${id}" name="${name}" value="${desc}"onclick="savedata(this)"></a>
+        <a href="detail.html"><img width="100%" alt="${name}" src=${img} id="${id}" name="${price}" value="${desc}" onclick="savedata(this)"></a>
         <div class="details">
           <h3>${name}</h3>
           <div class="price-quantity">
